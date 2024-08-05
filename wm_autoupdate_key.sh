d63cd00b25c05a189995cd1ea50eaa19198c48a8
Kevin Gonzalez <72703767+supmagie@users.noreply.github.com>
2024-08-05 22:37:36 +0900

    Update wm_autoupdate_key.sh

--- wm_autoupdate_key.sh
+++ wm_autoupdate_key.sh
@@ -2,7 +2,8 @@
 
 filename_key="vmware_key"
 sudo openssl req -new -x509 -newkey rsa:2048 -keyout ${filename_key}.priv -outform DER -out ${filename_key}.der -nodes -days 36500 -subj "/CN=VMware/"
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./${filename_key}.priv ./${filename_key}.der $(modinfo -n vmmon)
-sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./${filename_key}.priv ./${filename_key}.der $(modinfo -n vmnet)
+sudo /usr/src/kernel/`uname -r`/scripts/sign-file sha256 ./${filename_key}.priv ./${filename_key}.der $(modinfo -n vmmon)
+sudo /usr/src/kernel/`uname -r`/scripts/sign-file sha256 ./${filename_key}.priv ./${filename_key}.der $(modinfo -n vmnet)
+tail $(modinfo -n vmmon) | grep "Module signature appended"
 sudo mokutil --import ${filename_key}.der 
 echo "Now it's time for reboot, remember the password. You will get a blue screen after reboot choose 'Enroll MOK' -> 'Continue' -> 'Yes' -> 'enter password' -> 'OK' or 'REBOOT' " 
